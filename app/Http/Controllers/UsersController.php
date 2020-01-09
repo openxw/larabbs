1d701b76bf1bdb738e8e803af7d61f1a163f8b89
Alan <openxw@qq.com>
2020-01-09 12:55:59 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -13,4 +14,19 @@ class UsersController extends Controller
         # code...
         return view('users.show', compact('user'));
     }
+
+    public function edit(User $user)
+    {
+        # code...
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+    # code...
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
+
 }
