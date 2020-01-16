2b4643679677569a45eafaee5bbb8cee2cdd3d5d
Alan <openxw@qq.com>
2020-01-16 15:35:32 +0800

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -27,6 +27,11 @@ class TopicObserver
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
         }
-
     }
+
+     public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
 }
